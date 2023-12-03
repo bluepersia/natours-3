66af56f3e12274e94305d4f134cae39f35437eaa
User 1234 <user1234@mikeMac.local>
2023-12-03 16:51:11 -0600

    TourController basic

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require ('express');
+const globalErrorHandler = require ('./controllers/errorController');
+const AppError = require ('./utils/AppError');
+
 const app = express ();
 
+app.all ('*', (req, res, next) => { 
+    throw new AppError ('Route was not found', 404)
+});
 
+app.use (globalErrorHandler);
 
 module.exports = app;
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,6 @@
 const Tour = require ('../models/tourModel');
 const asyncHandler = require ('express-async-handler');
+const AppError = require ('../utils/AppError');
 
 exports.getAllTours = asyncHandler (async (req, res) =>
 {
@@ -9,6 +10,65 @@ exports.getAllTours = asyncHandler (async (req, res) =>
         status: 'success',
         data: {
             tours
+        }
+    })
+});
+
+exports.createTour = asyncHandler (async (req, res) => {
+
+    const tour = await Tour.create (req.body);
+
+    res.status (201).json ({
+        status: 'success',
+        data: {
+            tour
+        }
+    })
+})
+
+exports.getTour = asyncHandler (async (req, res) => {
+
+    const tour = await Tour.findById (req.params.id);
+
+    if (!tour)
+        throw new AppError ('No tour with that ID', 404);
+
+    res.status (200).json ({
+        status: 'success',
+        data: {
+            tour
+        }
+    })
+});
+
+
+exports.updateTour = asyncHandler (async (req, res) => {
+    
+    const tour = await Tour.findByIdAndUpdate (req.params.id, req.body, {new: true, runValidators: true});
+
+    if (!tour)
+        throw new AppError ('No tour with that ID', 404);
+
+    res.status (200).json ({
+        status: 'success',
+        data: {
+            tour
+        }
+    })
+});
+
+
+exports.deleteTour = asyncHandler (async (req, res) => {
+    
+    const tour = await Tour.findByIdAndDelete (req.params.id);
+
+    if (!tour)
+        throw new AppError ('No tour with that ID', 404);
+
+    res.status (204).json ({
+        status: 'success',
+        data: {
+            tour
         }
     })
 });
