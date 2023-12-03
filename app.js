77130448da0683df84b2eebbc7ee9b2dda891e2f
User 1234 <user1234@mikeMac.local>
2023-12-03 16:54:39 -0600

    Tour routes

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require ('express');
+const tourRouter = require ('./routes/tourRoutes');
 const globalErrorHandler = require ('./controllers/errorController');
 const AppError = require ('./utils/AppError');
 
 const app = express ();
+
+app.use ('/api/v1/tours', tourRouter);
 
 app.all ('*', (req, res, next) => { 
     throw new AppError ('Route was not found', 404)
